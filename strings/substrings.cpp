d9954ad9fd0923a51869e61dccda65ed92045fbd
indrajeeet223 <indrajeetkokate0@gmail.com>
2025-01-25 11:33:43 +0530

    codes of string operation

---
+++ strings/substrings.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+#include<algorithm> 
+using namespace std;
+int main()
+{
+    string v="aabcdefghi";
+    // substring(index,len or end );
+    cout<<v.substr(1);
+}
