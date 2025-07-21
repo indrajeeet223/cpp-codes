ec8080c52fbb0e2c2e7eff15db3cd55e4d1e3f3c
indrajeeet223 <indrajeetkokate0@gmail.com>
2025-07-22 00:17:54 +0530

    done

--- bitoperation/rightshiftoperator.cpp
+++ bitoperation/rightshiftoperator.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 int main()
 {
-int n=16;
-int result=n>>2;
+int n=16; // for int 32 bits are considered .
+int result=n>>3; //right shift ,=means divide by n/2**3 .
 cout<<result;
-// 10000 becomes 00100 it is 4 . 
+// 10000 becomes 00000100 it is 4 . 
 // it find the power of 2 equals to that number .
 
 }
